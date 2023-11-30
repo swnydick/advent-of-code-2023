d6c53643479e87c761fd9ec6121ac06052854899
Steven Nydick <steven@duolingo.com>
2023-11-30 09:39:37 -0500

    Setup initial skeleton of code structure

---
+++ 01/01-code.R
@@ -0,0 +1,21 @@
+#####################
+# AoC R Solution 01 #
+#                   #
+# Steven W. Nydick  #
+# 2023-12-01        #
+#####################
+
+# 1. Setup =====================================================================
+
+# read data into R (call it `inp` to reflect the file name)
+inp <- scan(
+  file = file.path("01", "01-input.txt"), # change based on new folder
+  what = double(),                        # change based on encoding
+  blank.lines.skip = FALSE
+)
+
+# 2. Part 1 ====================================================================
+
+
+# 3. Part 2 ====================================================================
+
